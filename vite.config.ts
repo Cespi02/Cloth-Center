95f61957a4d2d812913b0bf6f630f6a8727d457b
Luciano <luciano.cespedes02@gmail.com>
2025-05-26 17:35:56 -0300

    Arreglos para produccion

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Cloth-Center/', // 👈 esto es CLAVE
+  base: '/Cloth-Center/',
   plugins: [react()],
 })
