6ea2314836a36e1e0849de52f5f25dd704b48f79
Luciano <luciano.cespedes02@gmail.com>
2025-05-26 21:54:08 -0300

    vercell update

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Cloth-Center/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
 })
